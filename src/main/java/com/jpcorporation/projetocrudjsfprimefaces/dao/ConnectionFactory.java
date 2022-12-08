6317b21c7f0c319f32f1480946132216120e9a10
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-12-08 14:19:20 -0300

    created package dao and class connection factory

---
+++ src/main/java/com/jpcorporation/projetocrudjsfprimefaces/dao/ConnectionFactory.java
@@ -0,0 +1,17 @@
+
+package com.jpcorporation.projetocrudjsfprimefaces.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class ConnectionFactory {
+    
+    private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("cursoPU");
+    
+    public static EntityManager getEntityManager(){
+        return factory.createEntityManager();
+    }
+    
+}
