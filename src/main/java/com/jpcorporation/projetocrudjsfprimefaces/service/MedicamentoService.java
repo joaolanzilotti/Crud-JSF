2d5f911a181b0cca5492f67564746259385d39e0
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-12-09 09:16:23 -0300

    new Package service

---
+++ src/main/java/com/jpcorporation/projetocrudjsfprimefaces/service/MedicamentoService.java
@@ -0,0 +1,32 @@
+package com.jpcorporation.projetocrudjsfprimefaces.service;
+
+import com.jpcorporation.projetocrudjsfprimefaces.dao.DAO;
+import com.jpcorporation.projetocrudjsfprimefaces.medicamento.model.Medicamento;
+import com.jpcorporation.projetocrudjsfprimefaces.utils.ErroException;
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Inject;
+
+public class MedicamentoService implements Serializable {
+    
+    @Inject
+    private DAO<Medicamento> dao;
+    
+    public void salvar(Medicamento medicamento) throws ErroException{
+        
+        if(medicamento.getName().length() < 3){
+            throw new ErroException("O nome do medicamento não pode ter menos que 3 caracteres");
+        }
+        
+        dao.salvar(medicamento);
+    }
+    
+    public void remover(Medicamento medicamento){
+        dao.remover(Medicamento.class, medicamento.getId());
+    }
+    
+    public List<Medicamento> listaDeMedicamentos(){
+        return dao.buscarTodos("select m from Medicamento m order by m.name");
+    }
+    
+}
